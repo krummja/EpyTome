e1d6184c60e2d5da23e94fdc5140e810e347f7b2
Jonathan Crum <crumja4@gmail.com>
2021-01-08 10:25:06 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# EpyTome
+
+EpyTome is a plain text parser that generates LaTeX and BibTeX documents. It is intended for use by typesetters preparing manuscripts for academic publication.
---
+++ epytome/main.py
@@ -0,0 +1,12 @@
+"""
+EpyTome is a plain text parser for generating LaTeX and BibTeX documents,
+mainly for typesetting academic publications.
+"""
+
+
+def main():
+    """Application start."""
+
+
+if __name__ == '__main__':
+    main()
